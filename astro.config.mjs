d4b2802306edafb8eeae0f8b512aedf8e86761e0
Laerkebn <lbnonved@gmail.com>
2026-04-06 20:33:10 +0200

    opsætning

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
+
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
